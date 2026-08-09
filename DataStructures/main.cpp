fa1d8af92c7e003456c9883a2618038250855c8b
HaPpY <930333105@qq.com>
2026-08-09 17:26:12 +0800

    feat(build): 升级C++标准并添加ACM本地调试支持
    - 将CMAKE_CXX_STANDARD从14升级到20
    - 添加ACM_LOCAL宏定义用于本地调试
    - 移除不必要的include_directories指令
    - 添加main.cpp文件实现基础程序框架
    - 集成freopen用于ACM模式的输入输出重定向
    - 添加条件编译支持本地测试环境配置

---
+++ DataStructures/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+#ifdef ACM_LOCAL
+    freopen("data.in", "r", stdin);
+    freopen("data.out", "w", stdout);
+#endif
+
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
